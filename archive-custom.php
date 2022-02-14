48ff6e510c510ffec554982f8435af53ebbba333
Takuya Kawai <takuyakawai32@gmail.com>
2022-02-14 18:27:37 +0900

    add pagination

--- archive-custom.php
+++ archive-custom.php
@@ -47,6 +47,15 @@ Template Post Type: page
                 </div>
             <?php endfor; ?>
         </div>
+        <?php
+        $args = array(
+            'mid_size' => 1,
+            'prev_text' => '&lt;&lt;前へ',
+            'next_text' => '次へ&gt;&gt;',
+            'screen_reader_text' => ' ',
+        );
+        the_posts_pagination($args);
+        ?>
         <footer>
             <span>&copy; <?php bloginfo('name'); ?></span>
         </footer>
