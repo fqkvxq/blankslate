d05e652d83def56d4dbf6c6e0a5fbe6c0348a7c9
Takuya Kawai <takuyakawai32@gmail.com>
2022-02-14 18:42:31 +0900

    Revert "change"
    This reverts commit d46888400de316095d143cf48b268c34b2c89104.

--- archive.php
+++ archive.php
@@ -1,59 +1,12 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
-    <?php get_header(); ?>
-</head>
-
-<body>
-    <div class="wrapper">
-        <header>
-            <span>
-                <?php bloginfo('name'); ?>
-                <a href="<?php echo home_url() ?>"></a>
-            </span>
-        </header>
-        <div class="loop">
-            <?php
-            //$argsのプロパティを変えていく
-            $args = array(
-                'post_type' => 'post',
-                'posts_per_page' => 5,
-                'no_found_rows' => false,  //ページャーを使う時はfalseに。
-                'paged' => get_query_var('paged')
-            );
-            $the_query = new WP_Query($args);
-            ?>
-            <?php for ($i = 0; $i < $the_query->post_count; $i++) : ?>
-                <?php $post_url = $the_query->posts[$i]->guid; ?>
-                <div class="article">
-                    <div class="thumbnail">
-                        <img src="<?php echo get_the_post_thumbnail_url($the_query->posts[$i]); ?>" alt="">
-                    </div>
-                    <div class="title">
-                        <h2><?php echo ($the_query->posts[$i]->post_title); ?></h2>
-                    </div>
-                    <a href="<?php echo $post_url ?>"></a>
-                </div>
-                <?php
-                $pagination_args = array(
-                    'mid_size' => 1,
-                    'prev_text' => '&lt;&lt;前へ',
-                    'next_text' => '次へ&gt;&gt;',
-                    'screen_reader_text' => ' ',
-                );
-                the_posts_pagination($pagination_args);
-                ?>
-            <?php endfor; ?>
-        </div>
-        <footer>
-            <span>&copy; <?php bloginfo('name'); ?></span>
-        </footer>
-    </div>
-</body>
-
-</html>
+<?php get_header(); ?>
+<main id="content" role="main">
+<header class="header">
+<h1 class="entry-title" itemprop="name"><?php the_archive_title(); ?></h1>
+<div class="archive-meta" itemprop="description"><?php if ( '' != get_the_archive_description() ) { echo esc_html( get_the_archive_description() ); } ?></div>
+</header>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php get_template_part( 'entry' ); ?>
+<?php endwhile; endif; ?>
+<?php get_template_part( 'nav', 'below' ); ?>
+</main>
+<?php get_footer(); ?>
