2557301add424355061284d2c250beac56fbe27d
Takuya Kawai <takuyakawai32@gmail.com>
2022-02-14 18:33:35 +0900

    change

--- archive-custom.php
+++ archive-custom.php
@@ -45,16 +45,16 @@ Template Post Type: page
                     </div>
                     <a href="<?php echo $post_url ?>"></a>
                 </div>
+                <?php
+                $pagination_args = array(
+                    'mid_size' => 1,
+                    'prev_text' => '&lt;&lt;前へ',
+                    'next_text' => '次へ&gt;&gt;',
+                    'screen_reader_text' => ' ',
+                );
+                the_posts_pagination($pagination_args);
+                ?>
             <?php endfor; ?>
-            <?php
-            $args = array(
-                'mid_size' => 1,
-                'prev_text' => '&lt;&lt;前へ',
-                'next_text' => '次へ&gt;&gt;',
-                'screen_reader_text' => ' ',
-            );
-            the_posts_pagination($args);
-            ?>
         </div>
         <footer>
             <span>&copy; <?php bloginfo('name'); ?></span>
