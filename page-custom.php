730b22ee268d9b35b2c3613e87eede53d5e7b96f
Takuya Kawai <takuyakawai32@gmail.com>
2022-02-17 19:10:02 +0900

    🎉 add pagintion

--- page-custom.php
+++ page-custom.php
@@ -27,11 +27,13 @@ Template Post Type: page
         <div class="loop">
             <?php
             //$argsのプロパティを変えていく
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
             $args = array(
                 'post_type' => 'post',
-                'posts_per_page' => 100,
+                'posts_per_page' => 30,
                 'no_found_rows' => false,  //ページャーを使う時はfalseに。
-                'paged' => get_query_var('paged')
+                'paged' => $paged,
+                'orderby'     => 'date',
             );
             $the_query = new WP_Query($args);
             ?>
@@ -46,15 +48,28 @@ Template Post Type: page
                     </div>
                     <a href="<?php echo $post_url ?>"></a>
                 </div>
-                <?php
-                $pagination_args = array(
-                    'mid_size' => 1,
-                    'prev_text' => '&lt;&lt;前へ',
-                    'next_text' => '次へ&gt;&gt;',
-                    'screen_reader_text' => ' ',
-                );
-                the_posts_pagination($pagination_args);
-                ?>
+                <div class="pnavi">
+                    <?php //ページリスト表示処理
+                    global $wp_rewrite;
+                    $paginate_base = get_pagenum_link(1);
+                    if (strpos($paginate_base, '?') || !$wp_rewrite->using_permalinks()) {
+                        $paginate_format = '';
+                        $paginate_base = add_query_arg('paged', '%#%');
+                    } else {
+                        $paginate_format = (substr($paginate_base, -1, 1) == '/' ? '' : '/') .
+                            user_trailingslashit('page/%#%/', 'paged');
+                        $paginate_base .= '%_%';
+                    }
+                    echo paginate_links(array(
+                        'base' => $paginate_base,
+                        'format' => $paginate_format,
+                        'total' => $the_query->max_num_pages,
+                        'mid_size' => 1,
+                        'current' => ($paged ? $paged : 1),
+                        'prev_text' => '< 前へ',
+                        'next_text' => '次へ >',
+                    )); ?>
+                </div>
             <?php endfor; ?>
         </div>
         <footer>
