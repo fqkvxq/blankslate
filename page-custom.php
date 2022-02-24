54c792fd147c88c4b9b5e4d98c1b778756f55f9c
Takuya Kawai <takuyakawai32@gmail.com>
2022-02-24 21:00:19 +0900

    add description

--- page-custom.php
+++ page-custom.php
@@ -12,6 +12,7 @@ Template Post Type: page
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php bloginfo( 'description' ); ?>">
     <?php get_header(); ?>
 </head>
 
