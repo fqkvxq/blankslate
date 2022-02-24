be853fc6839f3b28f3e80d57b96291c2b6bfc246
Takuya Kawai <takuyakawai32@gmail.com>
2022-02-24 20:20:12 +0900

    disable skip to the content

--- functions.php
+++ functions.php
@@ -88,10 +88,10 @@ if (!function_exists('blankslate_wp_body_open')) {
   }
 }
 add_action('wp_body_open', 'blankslate_skip_link', 5);
-function blankslate_skip_link()
-{
-  echo '<a href="#content" class="skip-link screen-reader-text">' . esc_html__('Skip to the content', 'blankslate') . '</a>';
-}
+// function blankslate_skip_link()
+// {
+//   echo '<a href="#content" class="skip-link screen-reader-text">' . esc_html__('Skip to the content', 'blankslate') . '</a>';
+// }
 // add_filter('the_content_more_link', 'blankslate_read_more_link'); 無効
 function blankslate_read_more_link()
 {
